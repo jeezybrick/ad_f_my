3e28e01c12a15c81723bc203c75758ea516591a4
Andrey Statsenko <smooker14@gmail.com>
2015-11-26 11:57:56 +0200

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -3,8 +3,8 @@ from django.core.mail import send_mail
 
 def send_email_with_form_data(data):
 
-    email_from = 'smooker14@gmail.com'
-    email_to = 'smooker14@gmail.com'
+    email_from = 'info@adfits.com'
+    email_to = 'info@adfits.com'
     subject = 'demo_subject'
     phone = data.get('phone_0', default=None) + '-' + \
             data.get('phone_1', default=None)+'-' + \
