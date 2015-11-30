38e08d9c7feae845b322ccf592a639c5cd737e46
Andrey Statsenko <smooker14@gmail.com>
2015-11-30 20:54:50 +0200

    Create gulpfile.js

---
+++ static/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin');
+
+
+// compress images
+gulp.task('default', function() {
+
+});
+
+// compress images
+gulp.task('image', function() {
+    gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe (gulp.dest('images/compression'));
+});
