93dcbf435a18c1fffd116fcf651ece1cb7917150
Your Name <you@example.com>
2015-12-15 23:04:51 +0200

    add angular app and editpro controller;add nav;

---
+++ static/js/apps/app.js
@@ -0,0 +1,19 @@
+
+angular
+    .module('myApp', [
+        'ngResource'
+    ])
+    .config(function ($locationProvider, $httpProvider, $resourceProvider, $interpolateProvider) {
+
+        // CSRF Support
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+
+        // Force angular to use square brackets for template tag
+        // The alternative is using {% verbatim %}
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+
+
+    });
