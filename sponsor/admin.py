ad713c85910e1d2c990eeaff01b2d0fd18458a32
jeezybrick <smooker14@gmail.com>
2015-12-18 12:28:26 +0200

    add fiels for sponsor admin

--- sponsor/admin.py
+++ sponsor/admin.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageOps
 
 class SponsorAdmin(admin.ModelAdmin):
 
-    fields = ("name", )
+    fields = ("name", "logo", "country", "type", )
 '''
     form = SponsorForm
     exclude = ('token', 'password')
