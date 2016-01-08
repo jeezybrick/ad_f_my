ad64cbc2ebf00f6d1aa941ec6a3e91132df53b42
jeezybrick <smooker14@gmail.com>
2016-01-08 11:25:38 +0200

    edit(?) site category

--- static/js/controllers/PublisherEditSitesController.js
+++ static/js/controllers/PublisherEditSitesController.js
@@ -2,11 +2,13 @@ angular
     .module('myApp')
     .controller('PublisherEditSitesController', PublisherEditSitesController);
 
-function PublisherEditSitesController(Website, Publisher, $log, $state, $stateParams, Upload, $mdDialog) {
+function PublisherEditSitesController(Website, Publisher, $log, $state, $stateParams, Upload, $mdDialog, $mdConstant, $http) {
     var vm = this;
     vm.editWebsite = editWebsite;
     vm.clear = clear;
     vm.showJsTag = showJsTag;
+    vm.transformChip = transformChip;
+    vm.querySearch = querySearch;
 
     vm.publisher = {};
     vm.website = {};
@@ -14,14 +16,36 @@ function PublisherEditSitesController(Website, Publisher, $log, $state, $statePa
     vm.jsTag = '';
     vm.editWebsiteProcess = false;
 
+    vm.autocompleteDemoRequireMatch = false;
+    vm.selectedCategoryParent = null;
+    vm.selectedCategorySub = null;
+    vm.searchTextParent = null;
+    vm.searchTextSub = null;
+    vm.queryResult = {};
+    vm.keys = [$mdConstant.KEY_CODE.ENTER, $mdConstant.KEY_CODE.COMMA];
+
+    vm.category_default = [];
+    vm.category_sub = [];
+    //vm.type_of_category = ['default', 'sub'];
+
     /**
      * Get website detail
      */
     vm.website = Website.get({id: $stateParams.id}, function (response) {
         vm.publisher = Publisher.query(function (response) {
+
             vm.jsTag = '<div id="ad-adfits" pub_id="' + vm.publisher.id + '" web_id="' + vm.website.id + '">' +
                 '<script src="http://adfits.com/static/js/tag/ads.js"></script>' +
                 '</div>';
+
+            for(var i=0;i < vm.website.industry.length;i++){
+                if(angular.equals(vm.website.industry[i].type, 'default')){
+                    vm.category_default.push(vm.website.industry[i]);
+                }else{
+                    vm.category_sub.push(vm.website.industry[i]);
+                }
+            }
+
         }, function () {
 
         });
@@ -77,6 +101,8 @@ function PublisherEditSitesController(Website, Publisher, $log, $state, $statePa
             data: {
                 website_name:vm.website.website_name,
                 website_domain:vm.website.website_domain,
+                category_parent: vm.category_default,
+                category_sub: vm.category_sub,
                 twitter_name:vm.website.twitter_name,
                 facebook_page:vm.website.facebook_page,
             },
@@ -94,6 +120,28 @@ function PublisherEditSitesController(Website, Publisher, $log, $state, $statePa
     }
 
     function clear() {
+
+    }
+
+    function transformChip(chip, type) {
+        // If it is an object, it's already a known chip
+        if (angular.isObject(chip)) {
+            return chip;
+        }
+        // Otherwise, create a new one
+        return {industry_type: chip, type_public: 'new', type: type}
+    }
+
+    function querySearch(query, type) {
+
+        $http.get('/api/category?' + type + '=' + query).success(function (data) {
+            vm.queryResult = data;
+
+        }).error(function (error) {
+
+        });
+
+        return vm.queryResult;
 
     }
 
