09f338ed6faf3f51c43924397981a3c8cce04d77
jeezybrick <smooker14@gmail.com>
2016-01-13 15:14:59 +0200

    add smooth scroll

--- static/js/common.js
+++ static/js/common.js
@@ -54,3 +54,29 @@ $("form").submit(function (e) {
     return true;
 });
 
+$(document).ready(function () {
+
+    $("#how-it-works-link, .header-contact-as, #sidebar-how-it-works, #sidebar-contact-as").on("click", "a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+
+        //забираем идентификатор бока с атрибута href
+
+        var id = $(this).attr('href'),
+
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 700);
+
+    });
+
+});
